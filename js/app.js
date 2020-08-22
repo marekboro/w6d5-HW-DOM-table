c4f143a7f400f58251d5a8d28693737aa2637ade
marekboro <marek.borowicz83@gmail.com>
2020-08-22 15:27:38 +0100

    list now generating and clearing, created some function to separation of concerns - clear form fields is a thing now

--- js/app.js
+++ js/app.js
@@ -1,93 +1,96 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-   
-   // populateSection();
-   buildForm();
-   createListSpace();
+
+    buildForm();
+    createListSpace();
+    const addToList = document.querySelector('#indexBodyForm');
+    addToList.addEventListener('submit', addAListItems);
 });
 
 let aForm;
 
-const buildForm = function() {
+const buildForm = function () {
     console.log("building a form")
     const indexBody = document.querySelector('#formSection');
     const aForm = document.createElement('form');
-    aForm.id='indexBodyForm';
+    aForm.id = 'indexBodyForm';
 
-    const fromWrap=document.createElement('div');
-    fromWrap.id='wrapperForFormElements'
+    const fromWrap = document.createElement('div');
+    fromWrap.id = 'wrapperForFormElements'
 
-    const takeName = document.createElement('label');
-    takeName.for='name';
-    takeName.innerHTML="Name "
+    const formItem = document.createElement('div');
+
+    const nameFieldLabel = document.createElement('label');
+    nameFieldLabel.for = 'name';
+    nameFieldLabel.innerHTML = "Name ";
     const nameInput = document.createElement('input');
-    nameInput.type='text';
-    nameInput.id='enteredName';
+    nameInput.type = 'text';
+    nameInput.id = 'enteredName';
     nameInput.required='true'; //!! Seems like this will need to be fillded for submit form to work.
-    // <label for="title" class="primary">Title</label>
-    //       <input type="text" id="title" required/>
-    fromWrap.appendChild(takeName);
-    fromWrap.appendChild(nameInput);
+
+    const submitFrom = document.createElement('input');
+    submitFrom.id = "submitForm";
+    submitFrom.value = "Add to List";
+    submitFrom.type = "submit";
+
+    formItem.appendChild(nameFieldLabel);
+    formItem.appendChild(nameInput);
+
+
+    fromWrap.appendChild(formItem);
+    // fromWrap.appendChild(nameFieldLabel);
+    // fromWrap.appendChild(nameInput);
+
     aForm.appendChild(fromWrap);
+    aForm.appendChild(submitFrom);
     indexBody.appendChild(aForm);
-    
+
 };
 
-const createListSpace = function() {
+const createListSpace = function () {
     console.log("creating a space to print the list")
     const listAllocator = document.querySelector('#listSection');
-    const unorderedList =  document.createElement('ul');
-    unorderedList.id="UnorderedList"
+    const unorderedList = document.createElement('ul');
+    unorderedList.id = "UnorderedList"
     listAllocator.appendChild(unorderedList);
 };
 
-const addAListItem = function(event) {
+const addAListItems = function (event) {
+    addNameToList(event)
+    // addNameToList(event)
+    clearInputOptions()
+  
+};
+
+const addNameToList = function(event) {
+    event.preventDefault();
     console.log("trying to add to list")
-};
+    const list = document.querySelector('ul');
 
-
-
-const populateSection = function() {
-    console.log("populate");
-    const section = document.querySelector('section');
-    const aForm = document.createElement("p");
-    // aForm.innerHTML =  "";
-    // aForm.id = "theForm"
+    const newListItem = document.createElement("li");
     
-    const para1 = document.createElement("h3");
-    para1.textContent ="Testing 1";
+    const nameDiv = document.createElement('div');
+    nameDiv.id="nameDivInList";
+    const nameTitlie = document.createTextNode("Name: ");
     
-    const para2 = document.createElement("h4");
-    para2.textContent ="Testing 2";
+    const nameTyped = document.createTextNode('h1');
+    nameTyped.textContent = event.target.enteredName.value;
+
+    nameDiv.appendChild(nameTitlie);
+    nameDiv.appendChild(nameTyped);
+
+    newListItem.appendChild(nameDiv);
+
+    list.appendChild(newListItem);
     
-    const para3 = document.createElement("h5");
-    para3.textContent ="Testing 3"; 
+}
 
-    aForm.appendChild(para1);
-    aForm.appendChild(para2);
-    aForm.appendChild(para3);
-    section.appendChild(aForm);
+const clearInputOptions = function(){
+    document.getElementById('enteredName').value = "";
+}
 
 
-};
 
 
-// const populateForm(document.onload,() => {
-//     aForm = document.createElement('form');
-//     // aForm.innerHTML =  "";
-//     aForm.id = "theForm"
-    
-//     para1 = document.createElement('h3');
-//     para1.innerHTML ="Testing 1";
-    
-//     para2 = document.createElement('h4');
-//     para2.innerHTML ="Testing 2";
-    
-//     para3 = document.createElement('h5');
-//     para3.innerHTML ="Testing 3"; 
 
-//     aForm.appendChild(para1);
-//     aForm.appendChild(para2);
-//     aForm.appendChild(para3);
-// });
